2e99e6f149e9bf6025157f5be78fc5a82e8b9066
Carlos Rubén Acosta Herrera <carlosrubenah@gmail.com>
2021-10-17 15:18:10 -0500

    fk function added

--- S-DES.c
+++ S-DES.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int key[11] = {1,0,1,0,0,0,0,0,1,0}, key_p10[11], key_p10_LS1[11], key_p10_LS2[11], subkey1[9], 
-subkey2[9], plaintext[9] = {1,2,3,4,5,6,7,8}, left[5], right[5], ciphertext[9], iptext[9], 
-inverse_ip[9], xor_result[5], first[5] = {1,0,0,1}, second[5] = {0,1,0,1};
+int key[11] = {0,1,0,0,0,0,0,0,0,1}, key_p10[11], key_p10_LS1[11], key_p10_LS2[11], subkey1[9], 
+subkey2[9], plaintext[9] = {1,0,0,1,1,0,0,1}, left[5], right[5], ciphertext[9], iptext[9], 
+inverse_ip[9], xor_result[5], first[5], second[5], map_result[5], expansion[9], 
+SK_add1[5], SK_add2[5], S0[5][5]={{1,0,3,2},{3,2,1,0},{0,2,1,3},{3,1,3,2}},
+S1[5][5]={{0,1,2,3},{2,0,1,3},{3,0,1,0},{2,1,0,3}}, row, column, Sbox_result[5], s_decimal, s_binary[3];
+
 void P10(int key[], int key_p10[]){
 	int i;
 	printf("P10 working...");
@@ -62,6 +65,7 @@ void LS_2(int key_p10_LS1[], int key_p10_LS2[]){
 		printf("%d", key_p10_LS2[i]);	
 	}
 }
+
 void P8(int key[], int subkey[]){
 	int i;
 	printf("Generating subkey");
@@ -78,6 +82,7 @@ void P8(int key[], int subkey[]){
 		printf("%d", subkey[i]);	
 	}
 }
+
 void SubkeyGeneration(int key[]){
 	int i;
 	printf("Original key:\n\n");
@@ -96,6 +101,7 @@ void SubkeyGeneration(int key[]){
 	P8(key_p10_LS2, subkey2);
 	printf("\n\n");
 }
+
 void IP(int text[]){
 	printf("Initial permutation...");
 	int i;
@@ -112,6 +118,7 @@ void IP(int text[]){
 		printf("%d", iptext[i]);
 	}
 }
+
 void Inverse_IP(int text[]){
 	printf("\n\n");
 	printf("Undoing initial permutation...");
@@ -129,19 +136,17 @@ void Inverse_IP(int text[]){
 		printf("%d", inverse_ip[i]);
 	}
 }
+
 void XOR(int first[], int second[]){
-	printf("\n\n");
-	printf("Prueba XOR...");
-	printf("\n\n");
 	int i;
 	for(i=0;i<4;i++){
 		if(first[i] != second[i])
 			xor_result[i] = 1;
 		else
 			xor_result[i] = 0;
-		printf("%d", xor_result[i]);
 	}
 }
+
 void SW(int text[]){
 	printf("\n\n");
 	printf("Switch operation working...");
@@ -162,12 +167,152 @@ void SW(int text[]){
 		printf("%d", text[i]);
 	}
 }
+
+void fk(int iptext[], int subkey[]){
+	int i;
+	for(i=0;i<4;i++){
+		first[i] = iptext[i];
+	}
+	for(i=0;i<4;i++){
+		second[i] = iptext[i+4];
+	}
+	F_mapping(second, subkey);
+	XOR(first, map_result);
+	for(i=0;i<4;i++){
+		first[i] = xor_result[i];
+	}
+	for(i=0;i<8;i++){
+		if(i<4)
+			ciphertext[i] = first[i];
+		else
+			ciphertext[i] = second[i-4];
+	}	  
+}
+
+void EP(int expansion[], int R[]){
+    expansion[0] = R[3];
+    expansion[1] = R[0];
+    expansion[2] = R[1];
+    expansion[3] = R[2];
+    expansion[4] = R[1];
+    expansion[5] = R[2];
+    expansion[6] = R[3];
+    expansion[7] = R[0];
+}
+
+void binaryTodecimal(int binary[]){
+		if(binary[0] == 0){
+			if(binary[3] == 0){
+				row = 0;
+			}else{
+				row = 1;
+			}
+		}else{
+			if(binary[3] == 0){
+				row = 2;
+			}else{
+				row = 3;
+			}
+		}
+		if(binary[1] == 0){
+			if(binary[2] == 0){
+				column = 0;
+			}else{
+				column = 1;
+			}
+		}else{
+			if(binary[2] == 0){
+				column = 2;
+			}else{
+				column = 3;
+			}
+		}
+}
+
+void decimalToBinary(int decimal){
+	switch (decimal){
+		case 0:	s_binary[0] = 0; s_binary[1] = 0;
+				break;
+		case 1:	s_binary[0] = 0; s_binary[1] = 1;
+				break;
+		case 2:	s_binary[0] = 1; s_binary[1] = 0;
+				break;
+		case 3:	s_binary[0] = 1; s_binary[1] = 1;
+				break;
+	}
+		
+}
+
+void P4(int sbox[]){
+	map_result[0] = sbox[1];
+	map_result[1] = sbox[3];
+	map_result[2] = sbox[2];
+	map_result[3] = sbox[0];
+}
+
+void F_mapping(int R[], int SK[]){
+	int xor1[5], xor2[5], xor_subkey1[5], xor_subkey2[5], i;
+	EP(expansion, R);
+	for(i=0;i<8;i++){
+		if(i<4)
+			xor_subkey1[i] = SK[i];
+		else
+			xor_subkey2[i-4] = SK[i];
+	}
+	for(i=0;i<8;i++){
+		if(i<4)
+			xor1[i] = expansion[i];
+		else
+			xor2[i-4] = expansion[i];
+	}
+	XOR(xor1, xor_subkey1);
+	for(i=0;i<4;i++){
+		SK_add1[i] = xor_result[i];
+	}
+	XOR(xor2, xor_subkey2);
+	for(i=0;i<4;i++){
+		SK_add2[i] = xor_result[i];
+	}
+	binaryTodecimal(SK_add1);
+	s_decimal = S0[row][column];
+	decimalToBinary(s_decimal);
+	Sbox_result[0] = s_binary[0];
+	Sbox_result[1] = s_binary[1];
+	binaryTodecimal(SK_add2);
+	s_decimal = S1[row][column];
+	decimalToBinary(s_decimal);
+	Sbox_result[2] = s_binary[0];
+	Sbox_result[3] = s_binary[1];
+	P4(Sbox_result);
+}
+
 int main(){
+	int i;
 	SubkeyGeneration(key);
 	IP(plaintext);
-	Inverse_IP(iptext);
-	XOR(first, second);
-	SW(plaintext);
+	fk(iptext, subkey1);
+	SW(ciphertext);
+	fk(ciphertext, subkey2);
+	Inverse_IP(ciphertext);
+	printf("\n\n");
+	printf("Ciphertext:\t");
+	for(i=0;i<8;i++){
+		printf("%d", inverse_ip[i]);
+	}
+	printf("\n\n");
+	IP(inverse_ip);
+	fk(iptext, subkey2);
+	SW(ciphertext);
+	fk(ciphertext, subkey1);
+	Inverse_IP(ciphertext);
+	printf("\n\n");
+	printf("Plaintext:\t");
+	for(i=0;i<8;i++){
+		printf("%d", inverse_ip[i]);
+	}
+	//Inverse_IP(iptext);
+	//XOR(first, second);
+	//SW(plaintext); 
 	return 0;
 }
 
